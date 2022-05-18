b8dca5d2b8ca9ccb876e6ac5c7ffcbc9040b93dd
216 <216@git.com>
2022-05-18 14:59:52 +0800

    v2.1.0_01

--- src/test/java/ml/java/spring5/Spring5ApplicationTests.java
+++ src/test/java/ml/java/spring5/Spring5ApplicationTests.java
@@ -1,6 +1,7 @@
 package ml.java.spring5;
 
 import ml.java.spring5.demo1.User;
+import ml.java.spring5.demo2.Actor;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +15,13 @@ public class Spring5ApplicationTests {
         System.out.println("user constructed");
         user.hello();
         ((ClassPathXmlApplicationContext) applicationContext).close();
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Actor actor = applicationContext.getBean("actor", Actor.class);
+        actor.setName("LSP");
     }
 
 }
